59c49ad48356265c22c15147352874435e6e6db5
Magnus Jensen <magnuskahr@icloud.com>
2019-08-15 11:23:57 +0200

    Added public init for position

--- Sources/SkakKit/Position.swift
+++ Sources/SkakKit/Position.swift
@@ -10,6 +10,11 @@ import Foundation
 public struct Position {
     let file: File
     let rank: Rank
+    
+    public init(file: File, rank: Rank) {
+        self.file = file
+        self.rank = rank
+    }
 }
 
 extension Position: Equatable { }
